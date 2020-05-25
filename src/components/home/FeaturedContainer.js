ac0fc9b8e925c9dcd6ffc2b7fd3d6bce29aa1f4e
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-25 09:17:22 -0400

    Update FeaturedContainer.js

--- src/components/home/FeaturedContainer.js
+++ src/components/home/FeaturedContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import FeaturedVideoCard from "../videoCard/FeaturedVideoCard";
+import Container from "react-bootstrap/Container"
 
 //Container to display many cards of featured videos
 
 class FeaturedContainer extends Component {
   render() {
-    return <>FeaturedContainer</>
+    return <Container>FeaturedContainer</Container>
       // return (<>{this.props.videos.map(video => <FeaturedVideoCard key={video.id} video={video}/>)} </>);
   
   }
