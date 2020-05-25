6305085e39f9b5f264edb3389c6a2594f5355b39
duruchima <duruchima1@gmail.com>
2020-05-25 14:54:00 -0400

    index and show page working

--- src/components/videoCard/FeaturedVideoCard.js
+++ src/components/videoCard/FeaturedVideoCard.js
@@ -1,14 +1,12 @@
 import React from "react";
-
 const FeaturedVideoCard = (props) => {
-    return(
-		
-        <div>
-            {/* <img src={props.video.thumbnailURL} alt={props.video.title}></img>
+    return (
+        <div onClick={() => {props.onClick(props.video.id)}}>
+            <img src={props.video.thumbnailURL} width={props.video.thumbnail_width} height={props.video.thumbnail_height} alt={props.video.title}></img>
             <p>{props.video.title}</p>
             <p>{props.video.channelTitle}</p>
-            <span>Views: {props.video.viewCount}</span> */}
-            {/* <span>{props.video.uploadDate}</span> */}
+            <span>Views: {props.video.views}</span>
+            <span></span>
         </div>
     )
 
