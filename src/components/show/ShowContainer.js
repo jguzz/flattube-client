5fe0397dab2afed66cb55fe99d7a38cf45d91ae7
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-28 19:42:37 -0400

    added bootstrap styling for show

--- src/components/show/ShowContainer.js
+++ src/components/show/ShowContainer.js
@@ -3,22 +3,28 @@ import Video from "./Video/Video";
 import Details from "./Video/Details";
 import CommentsContainer from "./Video/comment/CommentsContainer.js";
 import RecommendedContainer from "./recommended/RecommendedContainer";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 class FlatTubeContainer extends Component {
   render() {
     return (
       <>
-        SHOW CONTAINER
+        <Container>
+          <Card>
+   
+              <Video video={this.props.video} />
         
-          <Video video={this.props.video} />
-          <Details
-            displayLikes={this.props.displayLikes}
-            handleLike={this.props.handleLike}
-            video={this.props.video}
-          />
-          {/* <CommentsContainer /> */}
-          {/* <RecommendedContainer/> */}
-        {" "}
+              <Details
+                displayLikes={this.props.displayLikes}
+                handleLike={this.props.handleLike}
+                video={this.props.video}
+              />
+
+            {/* <CommentsContainer /> */}
+            {/* <RecommendedContainer/> */}
+          </Card>
+        </Container>
       </>
     );
   }
