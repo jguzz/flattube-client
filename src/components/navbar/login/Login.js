cca0b76b79b1a7ada66aeffd517c99ac02341369
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-28 15:07:13 -0400

    login and sign up styled

--- src/components/navbar/login/Login.js
+++ src/components/navbar/login/Login.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
 class Login extends React.Component {
   state = {
@@ -13,22 +16,68 @@ class Login extends React.Component {
   render() {
     return (
       <>
-        <form onChange={this.handleLoginChange} onSubmit={(e) => this.props.onSubmitClick(e, this.state)}>
-          SignUp
-          <label>
-            username
-            <input
-              name="username"
-              type="text"
-              value={this.state.username}
-            />
-          </label>
-          <label>
-            Password
-            <input name="password" value={this.state.password} />
-          </label>
-          <input type="submit" />
-        </form>
+        <Container
+          style={{
+            alignContent: "center",
+            width: "75%",
+            height: "30em",
+            padding: "4.5%",
+          }}
+        >
+          <Card
+            style={{
+              background: "#F8F8F8",
+              width: "75%",
+              height: "100%",
+              margin: "auto",
+            }}
+          >
+            <Form
+              onChange={this.handleLoginChange}
+              onSubmit={(e) => this.props.onSubmitClick(e, this.state)}
+              style={{
+                alignContent: "center",
+                background: "#F8F8F8",
+                width: "75%",
+                margin: "auto",
+              }}
+            >
+              <Form.Group
+                style={{
+                  textAlign: "center",
+                  fontSize: "20pt",
+                  color: "#2AAED8",
+                }}
+              >
+                <Form.Label> Login</Form.Label>
+              </Form.Group>
+              <br />
+              <Form.Group>
+                <Form.Label>Username </Form.Label>
+                <Form.Control
+                  placeholder="Username"
+                  name="username"
+                  type="text"
+                  value={this.state.username}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label> Password </Form.Label>
+                <Form.Control
+                  placeholder="Password"
+                  name="password"
+                  value={this.state.password}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  type="submit"
+                  style={{ background: "#2AAED8", color: "white" }}
+                />
+              </Form.Group>
+            </Form>
+          </Card>
+        </Container>
       </>
     );
   }
--- src/components/navbar/login/SignUp.js
+++ src/components/navbar/login/SignUp.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
 function SignUp({
   handleChange,
@@ -8,38 +11,78 @@ function SignUp({
   age,
   img,
   name,
-  handleSignUp
+  handleSignUp,
 }) {
   return (
     <>
-      <form onChange={handleChange} onSubmit={handleSignUp}>
-        SignUp
-        <label>
-          Username
-          <input name="username" type="text" value={username} />
-        </label>
-        <label>
-          Password
-          <input name="password" value={password} />
-        </label>
-        <label>
-          Validate Password
-          <input name="validatePassword" value={validatePassword} />
-        </label>
-		<label>
-          Name
-          <input name="name" type="text" value={name} />
-        </label>
-        <label>
-          age
-          <input name="age" type="number" value={age} />
-        </label>
-        <label>
-          Profile Pic
-          <input name="img" type="text" value={img} />
-        </label>
-		<input type="submit"/>
-      </form>
+      <Container
+        style={{
+          alignContent: "center",
+          width: "75%",
+          height: "60em",
+          padding: "4.5%",
+        }}
+      >
+        <Card
+          style={{
+            background: "#F8F8F8",
+            width: "75%",
+            height: "100%",
+            margin: "auto",
+          }}
+        >
+          <Form
+            onChange={handleChange}
+            onSubmit={handleSignUp}
+            style={{
+              alignContent: "center",
+              background: "#F8F8F8",
+              width: "75%",
+              margin: "auto",
+            }}
+          >
+            <Form.Group
+              style={{
+                textAlign: "center",
+                fontSize: "20pt",
+                color: "#2AAED8",
+              }}
+            >
+              <Form.Label> Sign Up</Form.Label>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Username</Form.Label>
+              <Form.Control name="username" type="text" value={username} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control name="password" value={password} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Validate Password</Form.Label>
+              <Form.Control name="validatePassword" value={validatePassword} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Name</Form.Label>
+              <Form.Control name="name" type="text" value={name} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>age</Form.Label>
+              <Form.Control name="age" type="number" value={age} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Profile Pic</Form.Label>
+              <Form.Control name="img" type="text" value={img} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="submit"
+                style={{ background: "#2AAED8", color: "white" }}
+              />
+            </Form.Group>
+          </Form>
+        </Card>
+      </Container>
     </>
   );
 }
