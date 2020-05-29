2daae7cb63f2ab35709f7a4189f99d64e737ed95
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-29 06:49:11 -0400

    log in log out styled

--- src/components/navbar/login/Login.js
+++ src/components/navbar/login/Login.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 class Login extends React.Component {
   state = {
     username: "",
@@ -10,25 +15,93 @@ class Login extends React.Component {
       console.log(this.state)
     );
   };
+
   render() {
     return (
       <>
-        <form onChange={this.handleLoginChange} onSubmit={(e) => this.props.onSubmitClick(e, this.state)}>
-          SignUp
-          <label>
-            username
-            <input
-              name="username"
-              type="text"
-              value={this.state.username}
-            />
-          </label>
-          <label>
-            Password
-            <input name="password" value={this.state.password} />
-          </label>
-          <input type="submit" />
-        </form>
+        <Container
+          style={{
+            height: "100em",
+            alignContent: "center",
+            margin: "auto",
+            padding: "10em",
+          }}
+        >
+          <form
+            style={{ flexGrow: 1, minHeight: "40em", margin: "auto" }}
+            onChange={this.handleLoginChange}
+            onSubmit={(e) => this.props.onSubmitClick(e, this.state)}
+          >
+            <Grid container spacing={10}>
+              <Grid item xs={12}>
+                <Typography
+                  style={{
+                    color: "#33ccff",
+                    textAlign: "center",
+                    fontSize: "3em",
+                  }}
+                >
+                  Log In{" "}
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: "10%", marginRight: "10%" }}
+              >
+                <TextField
+                  style={{ margin: "auto", width: "100%", textAlign: "center" }}
+                  label="Username"
+                  name="username"
+                  type="text"
+                  value={this.state.username}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: "10%", marginRight: "10%" }}
+              >
+                <TextField
+                  style={{ margin: "auto", width: "100%", textAlign: "center" }}
+                  label="Password"
+                  name="password"
+                  value={this.state.password}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                style={{
+                  marginLeft: "15%",
+                  marginRight: "15%",
+                  textAlign: "center",
+                }}
+              >
+                <Button
+                  style={{ margin: "auto", width: "70%", color: "#33ccff" }}
+                  size="large"
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid
+                item
+                style={{
+                  textAlign: "center",
+                  margin: "auto",
+                  marginTop: "2em",
+                }}
+              >
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </Container>
       </>
     );
   }
--- src/components/navbar/login/SignUp.js
+++ src/components/navbar/login/SignUp.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 function SignUp({
   handleChange,
@@ -8,38 +14,121 @@ function SignUp({
   age,
   img,
   name,
-  handleSignUp
+  handleSignUp,
 }) {
   return (
     <>
-      <form onChange={handleChange} onSubmit={handleSignUp}>
-        SignUp
-        <label>
-          Username
-          <input name="username" type="text" value={username} />
-        </label>
-        <label>
-          Password
-          <input name="password" value={password} />
-        </label>
-        <label>
-          Validate Password
-          <input name="validatePassword" value={validatePassword} />
-        </label>
-		<label>
-          Name
-          <input name="name" type="text" value={name} />
-        </label>
-        <label>
-          age
-          <input name="age" type="number" value={age} />
-        </label>
-        <label>
-          Profile Pic
-          <input name="img" type="text" value={img} />
-        </label>
-		<input type="submit"/>
-      </form>
+      <Container
+        style={{
+          height: "100em",
+          alignContent: "center",
+          margin: "auto",
+          padding: "10em",
+        }}
+      >
+        <form
+          onChange={handleChange}
+          onSubmit={handleSignUp}
+          style={{ flexGrow: 1, minHeight: "40em", margin: "auto" }}
+        >
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <Typography
+                style={{
+                  color: "#33ccff",
+                  textAlign: "center",
+                  fontSize: "3em",
+                }}
+              >
+                SignUp
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ marginLeft: "10%", marginRight: "10%" }}
+            >
+              <TextField
+                style={{ margin: "auto", width: "100%", textAlign: "center" }}
+                label="Username"
+                name="username"
+                type="text"
+                value={username}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ marginLeft: "10%", marginRight: "10%" }}
+            >
+              <TextField
+                style={{ margin: "auto", width: "100%", textAlign: "center" }}
+                label="Password"
+                name="password"
+                value={password}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ marginLeft: "10%", marginRight: "10%" }}
+            >
+              <TextField
+                style={{ margin: "auto", width: "100%", textAlign: "center" }}
+                label="Validate Password"
+                name="validatePassword"
+                value={validatePassword}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ marginLeft: "10%", marginRight: "10%" }}
+            >
+              <TextField
+                style={{ margin: "auto", width: "100%", textAlign: "center" }}
+                label="Name"
+                name="name"
+                type="text"
+                value={name}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ marginLeft: "10%", marginRight: "10%" }}
+            >
+              <TextField
+                style={{ margin: "auto", width: "100%", textAlign: "center" }}
+                label="Age"
+                name="age"
+                type="number"
+                value={age}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ marginLeft: "10%", marginRight: "10%" }}
+            >
+              <TextField
+                style={{ margin: "auto", width: "100%", textAlign: "center" }}
+                label="Profile Pic"
+                name="img"
+                type="text"
+                value={img}
+              />
+            </Grid>
+
+            <Button
+              style={{ margin: "auto", width: "70%", color: "#33ccff" }}
+              size="large"
+            >
+              Submit
+            </Button>
+          </Grid>
+        </form>
+      </Container>
     </>
   );
 }
