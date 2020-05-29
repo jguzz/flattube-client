8ff061e09620b1e9aea4b844d2d8b73eb286d59c
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-29 10:26:22 -0400

    fixed show for results

--- src/components/search/VideoCard.js
+++ src/components/search/VideoCard.js
@@ -1,21 +1,37 @@
 import React from "react";
 // import Video from "../show/Video/Video";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
 
 //Represents a card for each video in the results container.
 
 function VideoCard(props) {
-
-    return (
-      <>
-        <div onClick={() => {props.onClick(props.video.id)}}>
-            <img src={props.video.thumbnailURL} width={props.video.thumbnail_width} height={props.video.thumbnail_height} alt={props.video.title}></img>
-            <p>{props.video.title}</p>
-            <p>{props.video.channelTitle}</p>
-            <span>Views: {props.video.views}</span>
-        </div>
-      </>
-    );
-  
+  return (
+    <>
+      <Card
+        style={{ margin: "auto", background: "black", maxWidth:"50em" }}
+        onClick={() => {
+          props.onClick(props.video.id);
+        }}
+      >
+        <img
+          src={props.video.thumbnailURL}
+          width={"100%"}
+          height={"100%"}
+          alt={props.video.title}
+        ></img>
+        <Typography style={{ color: "white", fontSize: "16pt" }}>
+          {props.video.title}
+        </Typography>
+        <Typography style={{ color: "#BBBBBB" }}>
+          {props.video.channelTitle}
+        </Typography>
+        <Typography style={{ color: "#BBBBBB" }}>
+          Views: {props.video.views}
+        </Typography>
+      </Card>
+    </>
+  );
 }
 
 export default VideoCard;
